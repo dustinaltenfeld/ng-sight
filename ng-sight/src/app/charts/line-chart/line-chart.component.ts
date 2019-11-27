7b06ac97d989237015c925621b554ee6044f798d
Dr.D <altenfeld@gmail.com>
2019-11-28 00:36:52 +0100

    Add Line Charts

--- ng-sight/src/app/charts/line-chart/line-chart.component.ts
+++ ng-sight/src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {LINE_CHART_COLORS} from '../../shared/chart.colors';
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  {data: [32,45,45,94,45,87,11,32],label: 'Users'},
+  {data: [72,45,45,90,45,87,10,32],label: 'NewUsers'},
+  {data: [12,45,45,94,45,87,11,32],label: 'DeletedUsers'}
+];
+
+const LINE_CHART_LABELS: string[] = [
+  'Week1',
+  'Week2',
+  'Week3',
+  'Week4',
+  'Week5',
+  'Week6',
+  'Week7',
+  'Week8'
+];
+
 
 @Component({
   selector: 'app-line-chart',
@@ -8,8 +27,19 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+  lineChartLegend: true;
+  lineChartType = 'line';
+  lineChartColors = LINE_CHART_COLORS;
 
   ngOnInit() {
   }
+
+
 
 }
---
+++ ng-sight/src/app/shared/chart.colors.ts
@@ -0,0 +1,29 @@
+export const LINE_CHART_COLORS = [
+    {
+      backgroundColor: 'rgba(6,214,160,0.2)',
+      borderColor: 'rgba(0,200,140,0.5)',
+      pointBackgroundColor: '#000',
+      pointBorderColor: '#000',
+      pointHoverBackgroundColor: '#555',
+      pointHoverBorderColor: '#555'
+  
+    },
+    {
+      backgroundColor: 'rgba(255,214,160,0.2)',
+      borderColor: 'rgba(240,200,140,0.5)',
+      pointBackgroundColor: '#000',
+      pointBorderColor: '#000',
+      pointHoverBackgroundColor: '#555',
+      pointHoverBorderColor: '#555'
+  
+    },
+    {
+      backgroundColor: 'rgba(15,214,133,0.2)',
+      borderColor: 'rgba(0,64,128,0.5)',
+      pointBackgroundColor: '#000',
+      pointBorderColor: '#000',
+      pointHoverBackgroundColor: '#555',
+      pointHoverBorderColor: '#555'
+  
+    }
+  ];
